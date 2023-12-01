5ed59b74d99d7da6b625a5f671e015b08316cd0d
crisLeo-P01 <cristianleopaiva@gmail.com>
2023-12-01 10:07:58 -0300

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -11,88 +11,88 @@ const api = axios.create({
 })
 
 //////////////////////////////////> UTILS
-function createMovies ( movies, container ) {
+function createMovies(movies, container) {
   container.innerHTML = '',
-  movies.forEach(( movie ) => {
-    const containMovie = document.createElement( 'div' );
-    containMovie.classList.add( 'contain-movie' );
-    containMovie.addEventListener( 'click', () => {
-      location.hash = '#movie=' + movie.id;
+    movies.forEach((movie) => {
+      const containMovie = document.createElement('div');
+      containMovie.classList.add('contain-movie', 'placeholder-loading');
+      containMovie.addEventListener('click', () => {
+        location.hash = '#movie=' + movie.id;
+      })
+
+      const movieImg = ('https://image.tmdb.org/t/p/w300' + movie.poster_path);
+      const titleMovie = movie.title;
+      const voteAverage = movie.vote_average.toFixed(1);
+
+      containMovie.innerHTML = `
+      <img src=${movieImg} class="movie-img">
+      <div class="container name-valor-movie d-flex align-items-center">
+        <div class="row flex-column">
+          <div class="col-lg-12">
+            <span class="cartel-name">${titleMovie}</span>
+          </div>
+          <div class="col-lg-12">
+            <div class="valoration">
+              <img src="./src/images/ico-star.png">
+              <span class="puntuacion-number">${voteAverage}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+      container.appendChild(containMovie);
     })
-
-    const movieImg = ('https://image.tmdb.org/t/p/w300' + movie.poster_path );
-    const titleMovie = movie.title;
-    const voteAverage = movie.vote_average.toFixed(1);
-
-    containMovie.innerHTML = `
-      <img src=${ movieImg } class="movie-img">
-      <div class="container name-valor-movie d-flex align-items-center">
-        <div class="row flex-column">
-          <div class="col-lg-12">
-            <span class="cartel-name">${ titleMovie }</span>
-          </div>
-          <div class="col-lg-12">
-            <div class="valoration">
-              <img src="./src/images/ico-star.png">
-              <span class="puntuacion-number">${ voteAverage }</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    `
-    container.appendChild( containMovie );
-  })
 }
 
-function createCategories( categories, container ) {
+function createCategories(categories, container) {
   container.innerHTML = '';
 
-  categories.forEach(( category ) => {
-    const containCategory = document.createElement( 'div' );
-    containCategory.classList.add( 'category-container' );
-    
-    const categoryTitle = document.createElement( 'h3' );
-    categoryTitle.classList.add( 'category-title' );
-    categoryTitle.setAttribute( 'id', 'id' + category.id );
-    categoryTitle.addEventListener( 'click', () => {
-      location.hash = `#category=${ category.id }-${ category.name }`;
+  categories.forEach((category) => {
+    const containCategory = document.createElement('div');
+    containCategory.classList.add('category-container');
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id', 'id' + category.id);
+    categoryTitle.addEventListener('click', () => {
+      location.hash = `#category=${category.id}-${category.name}`;
     });
-    
-    const categoryTitleText = document.createTextNode( category.name );
-    
-    categoryTitle.appendChild( categoryTitleText );
-    containCategory.appendChild( categoryTitle );
-    container.appendChild( containCategory );
+
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    containCategory.appendChild(categoryTitle);
+    container.appendChild(containCategory);
   })
 }
 
-function genericMovies( movies, container ) {
+function genericMovies(movies, container) {
   container.innerHTML = '';
 
-  movies.forEach(( movie ) => {
-    const containMovie = document.createElement( 'div' );
-    containMovie.classList.add( 'contain-movie-general' );
-    const movieImg = ('https://image.tmdb.org/t/p/w300' + movie.poster_path );
+  movies.forEach((movie) => {
+    const containMovie = document.createElement('div');
+    containMovie.classList.add('contain-movie-general');
+    const movieImg = ('https://image.tmdb.org/t/p/w300' + movie.poster_path);
     const titleMovie = movie.title;
     const voteAverage = movie.vote_average.toFixed(1);
 
     containMovie.innerHTML = `
-      <img src=${ movieImg } class="movie-img">
+      <img src=${movieImg} class="movie-img">
       <div class="container name-valor-movie d-flex align-items-center">
         <div class="row flex-column">
           <div class="col-lg-12">
-            <span class="cartel-name">${ titleMovie }</span>
+            <span class="cartel-name">${titleMovie}</span>
           </div>
           <div class="col-lg-12">
             <div class="valoration">
               <img src="./src/images/ico-star.png">
-              <span class="puntuacion-number">${ voteAverage }</span>
+              <span class="puntuacion-number">${voteAverage}</span>
             </div>
           </div>
         </div>
       </div>
     `
-    container.appendChild( containMovie );
+    container.appendChild(containMovie);
   })
 }
 
@@ -100,113 +100,113 @@ function genericMovies( movies, container ) {
 //////////////////////////////////> MOVIES CATEGORIES
 
 async function moviesForCategories() {
-  const { data } = await api( 'genre/movie/list' );
+  const { data } = await api('genre/movie/list');
   const categories = data.genres;
-  console.log( categories );
+  console.log(categories);
 
-  createCategories( categories, categoriesPreviewList );
+  createCategories(categories, categoriesPreviewList);
 }
 
 
 //////////////////////////////////> MOVIES TRENDING PREVIEW
 
 async function getMoviesTrendingPreview() {
-  const { data } = await api( 'movie/now_playing' );
+  const { data } = await api('movie/now_playing');
   const movies = data.results;
 
-  createMovies( movies, trendingMoviesPreviewList );
+  createMovies(movies, trendingMoviesPreviewList);
 }
 
 
 //////////////////////////////////> MOVIES TRENDING
 
 async function getMoviesTrending() {
-  const { data } = await api( 'movie/now_playing' );
+  const { data } = await api('movie/now_playing');
   const movies = data.results;
 
-  genericMovies( movies, genericSection );
+  genericMovies(movies, genericSection);
 }
 
 
 //////////////////////////////////> MOVIES POPULARS PREVIEW
 
 async function getMoviesPopularPreview() {
-  const { data } = await api( 'movie/popular' );
+  const { data } = await api('movie/popular');
   const moviesPopular = data.results;
   console.log({ data, moviesPopular });
 
-  createMovies( moviesPopular, popularMoviesPreviewList );
+  createMovies(moviesPopular, popularMoviesPreviewList);
 }
 
 
 //////////////////////////////////> MOVIES POPULARS
 
 async function getMoviesPopular() {
-  const { data } = await api( 'movie/popular' );
+  const { data } = await api('movie/popular');
   const moviesPopular = data.results;
   console.log({ data, moviesPopular });
 
-  genericMovies( moviesPopular, genericSection );
+  genericMovies(moviesPopular, genericSection);
 }
 
 
 //////////////////////////////////> MOVIES BY CATEGORY
 
-async function getMoviesByCategory( id ) {
-  const { data } = await api( 'discover/movie', {
+async function getMoviesByCategory(id) {
+  const { data } = await api('discover/movie', {
     params: {
       with_genres: id,
     }
   });
   const movies = data.results;
-  
-  genericMovies( movies, categoriesMoviesPreviewList )
+
+  genericMovies(movies, categoriesMoviesPreviewList)
 }
 
 
 //////////////////////////////////> MOVIES BY SEARCH
 
-async function getMoviesBySearch( query ) {
-  const { data } = await api( 'search/movie', {
+async function getMoviesBySearch(query) {
+  const { data } = await api('search/movie', {
     params: {
       query,
     }
   });
   const movies = data.results;
 
-  genericMovies( movies, categoriesMoviesPreviewList );
+  genericMovies(movies, categoriesMoviesPreviewList);
 }
 
 
 //////////////////////////////////> MOVIES BY ID
 
-async function getMovieById( id ) {
-  
-  const { data: movie } = await api( 'movie/' + id );
+async function getMovieById(id) {
+
+  const { data: movie } = await api('movie/' + id);
 
   const movieBackdropImg = 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path;
   movieBackground.style.background = `
     linear-gradient(0deg, rgba(19, 14, 24, 0.45) 0%, rgba(19, 14, 24, 0.45) 100%),
-    url(${ movieBackdropImg })
+    url(${movieBackdropImg})
   `
   movieBackground.style.backgroundRepeat = 'no-repeat';
   movieBackground.style.backgroundSize = 'cover';
-  
+
   const movieDetailBack = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
   console.log(movieDetailBack);
 
   // Eliminamos la imagen anterior si existe
-  const previousImg = containerMovieImg.querySelector( '.movie-img-tapa' );
-  if( previousImg ) {
-    containerMovieImg.removeChild( previousImg );
+  const previousImg = containerMovieImg.querySelector('.movie-img-tapa');
+  if (previousImg) {
+    containerMovieImg.removeChild(previousImg);
   }
-  
-  const movieDetailImg = document.createElement( 'img' );
 
-  movieDetailImg.classList.add( 'movie-img-tapa' );
-  movieDetailImg.src = `${ movieDetailBack }`
+  const movieDetailImg = document.createElement('img');
 
-  containerMovieImg.appendChild( movieDetailImg );
+  movieDetailImg.classList.add('movie-img-tapa');
+  movieDetailImg.src = `${movieDetailBack}`
+
+  containerMovieImg.appendChild(movieDetailImg);
 
   releaseTime.textContent = movie.release_date;
   runTime.textContent = movie.runtime + 'min';
@@ -214,17 +214,17 @@ async function getMovieById( id ) {
   movieDetailDescription.textContent = movie.overview;
   movieDetailScore.textContent = movie.vote_average.toFixed(1);
 
-  createCategories( movie.genres, movieDetailCategoriesList )
+  createCategories(movie.genres, movieDetailCategoriesList)
 
-  getRelatedMoviesId( id );
+  getRelatedMoviesId(id);
 
 }
 
-async function getRelatedMoviesId( id ) {
-  const { data } = await api( `movie/${ id }/recommendations` );
+async function getRelatedMoviesId(id) {
+  const { data } = await api(`movie/${id}/recommendations`);
   const relatedMovies = data.results;
 
-  createMovies( relatedMovies, relatedMoviesContainer )
+  createMovies(relatedMovies, relatedMoviesContainer)
 }
 
 
